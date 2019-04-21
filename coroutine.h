e005d4ee12d5e4bcbbca58ce08da9c1d2b494d76
Away <875229565@qq.com>
2019-04-21 16:51:20 +0800

    Passed some test
    Passed some test

--- coroutine.h
+++ coroutine.h
@@ -29,6 +29,7 @@
 #include <cstdio>
 #include <cassert>
 
+#include <functional>
 #include <string>
 #include <vector>
 #include <list>
@@ -57,166 +58,166 @@ typedef unsigned routine_t;
 
 struct Routine
 {
-	std::function<void()> func;
-	bool finished;
-	LPVOID fiber;
+    std::function<void()> func;
+    bool finished;
+    LPVOID fiber;
 
-	Routine(std::function<void()> f)
-	{
-		func = f;
-		finished = false;
-		fiber = nullptr;
-	}
+    Routine(std::function<void()> f)
+    {
+        func = f;
+        finished = false;
+        fiber = nullptr;
+    }
 
-	~Routine()
-	{
-		DeleteFiber(fiber);
-	}
+    ~Routine()
+    {
+        DeleteFiber(fiber);
+    }
 };
 
 struct Ordinator
 {
-	std::vector<Routine *> routines;
-	std::list<routine_t> indexes;
-	routine_t current;
-	size_t stack_size;
-	LPVOID fiber;
+    std::vector<Routine *> routines;
+    std::list<routine_t> indexes;
+    routine_t current;
+    size_t stack_size;
+    LPVOID fiber;
 
-	Ordinator(size_t ss = STACK_LIMIT)
-	{
-		current = 0;
-		stack_size = ss;
-		fiber = ConvertThreadToFiber(nullptr);
-	}
+    Ordinator(size_t ss = STACK_LIMIT)
+    {
+        current = 0;
+        stack_size = ss;
+        fiber = ConvertThreadToFiber(nullptr);
+    }
 
-	~Ordinator()
-	{
-		for (auto &routine : routines)
-			delete routine;
-	}
+    ~Ordinator()
+    {
+        for (auto &routine : routines)
+            delete routine;
+    }
 };
 
 thread_local static Ordinator ordinator;
 
 inline routine_t create(std::function<void()> f)
 {
-	Routine *routine = new Routine(f);
+    Routine *routine = new Routine(f);
 
-	if (ordinator.indexes.empty())
-	{
-		ordinator.routines.push_back(routine);
-		return ordinator.routines.size();
-	}
-	else
-	{
-		routine_t id = ordinator.indexes.front();
-		ordinator.indexes.pop_front();
-		assert(ordinator.routines[id - 1] == nullptr);
-		ordinator.routines[id - 1] = routine;
-		return id;
-	}
+    if (ordinator.indexes.empty())
+    {
+        ordinator.routines.push_back(routine);
+        return ordinator.routines.size();
+    }
+    else
+    {
+        routine_t id = ordinator.indexes.front();
+        ordinator.indexes.pop_front();
+        assert(ordinator.routines[id - 1] == nullptr);
+        ordinator.routines[id - 1] = routine;
+        return id;
+    }
 }
 
 inline void destroy(routine_t id)
 {
-	Routine *routine = ordinator.routines[id - 1];
-	assert(routine != nullptr);
+    Routine *routine = ordinator.routines[id - 1];
+    assert(routine != nullptr);
 
-	delete routine;
-	ordinator.routines[id - 1] = nullptr;
-	ordinator.indexes.push_back(id);
+    delete routine;
+    ordinator.routines[id - 1] = nullptr;
+    ordinator.indexes.push_back(id);
 }
 
 inline void __stdcall entry(LPVOID lpParameter)
 {
-	routine_t id = ordinator.current;
-	Routine *routine = ordinator.routines[id - 1];
-	assert(routine != nullptr);
+    routine_t id = ordinator.current;
+    Routine *routine = ordinator.routines[id - 1];
+    assert(routine != nullptr);
 
-	routine->func();
+    routine->func();
 
-	routine->finished = true;
-	ordinator.current = 0;
+    routine->finished = true;
+    ordinator.current = 0;
 
-	SwitchToFiber(ordinator.fiber);
+    SwitchToFiber(ordinator.fiber);
 }
 
 inline int resume(routine_t id)
 {
-	assert(ordinator.current == 0);
+    assert(ordinator.current == 0);
 
-	Routine *routine = ordinator.routines[id - 1];
-	if (routine == nullptr)
-		return -1;
+    Routine *routine = ordinator.routines[id - 1];
+    if (routine == nullptr)
+        return -1;
 
-	if (routine->finished)
-		return -2;
+    if (routine->finished)
+        return -2;
 
-	if (routine->fiber == nullptr)
-	{
-		routine->fiber = CreateFiber(ordinator.stack_size, entry, 0);
-		ordinator.current = id;
-		SwitchToFiber(routine->fiber);
-	}
-	else
-	{
-		ordinator.current = id;
-		SwitchToFiber(routine->fiber);
-	}
+    if (routine->fiber == nullptr)
+    {
+        routine->fiber = CreateFiber(ordinator.stack_size, entry, 0);
+        ordinator.current = id;
+        SwitchToFiber(routine->fiber);
+    }
+    else
+    {
+        ordinator.current = id;
+        SwitchToFiber(routine->fiber);
+    }
 
-	return 0;
+    return 0;
 }
 
 inline void yield()
 {
-	routine_t id = ordinator.current;
-	Routine *routine = ordinator.routines[id - 1];
-	assert(routine != nullptr);
+    routine_t id = ordinator.current;
+    Routine *routine = ordinator.routines[id - 1];
+    assert(routine != nullptr);
 
-	ordinator.current = 0;
-	SwitchToFiber(ordinator.fiber);
+    ordinator.current = 0;
+    SwitchToFiber(ordinator.fiber);
 }
 
 inline routine_t current()
 {
-	return ordinator.current;
+    return ordinator.current;
 }
 
 #if 0
 template<typename Function, typename ... Args>
-inline std::result_of_t<std::decay_t<Function>()>
+inline typename std::result_of<Function(Args...)>::type
 await(Function &&func, Args && ... args)
 {
-	auto future = std::async(std::launch::async, func, std::forward<Args>(args)...);
-	std::future_status status = future.wait_for(std::chrono::milliseconds(0));
+    auto future = std::async(std::launch::async, func, std::forward<Args>(args)...);
+    std::future_status status = future.wait_for(std::chrono::milliseconds(0));
 
-	while (status == std::future_status::timeout)
-	{
-		if (ordinator.current != 0)
-			yield();
+    while (status == std::future_status::timeout)
+    {
+        if (ordinator.current != 0)
+            yield();
 
-		status = future.wait_for(std::chrono::milliseconds(0));
-	}
-	return future.get();
+        status = future.wait_for(std::chrono::milliseconds(0));
+    }
+    return future.get();
 }
 #endif
 
 #if 1
 template<typename Function, typename ... Args>
-inline std::result_of_t<std::decay_t<Function>()>
+inline std::result_of_t<Function(Args...)>
 await(Function &&func, Args && ... args)
 {
-	auto future = std::async(std::launch::async, func, std::forward<Args>(args)...);
-	std::future_status status = future.wait_for(std::chrono::milliseconds(0));
+    auto future = std::async(std::launch::async, func, std::forward<Args>(args)...);
+    std::future_status status = future.wait_for(std::chrono::milliseconds(0));
 
-	while (status == std::future_status::timeout)
-	{
-		if (ordinator.current != 0)
-			yield();
+    while (status == std::future_status::timeout)
+    {
+        if (ordinator.current != 0)
+            yield();
 
-		status = future.wait_for(std::chrono::milliseconds(0));
-	}
-	return future.get();
+        status = future.wait_for(std::chrono::milliseconds(0));
+    }
+    return future.get();
 }
 #endif
 
@@ -224,165 +225,165 @@ await(Function &&func, Args && ... args)
 
 struct Routine
 {
-	std::function<void()> func;
-	char *stack;
-	bool finished;
-	ucontext_t ctx;
+    std::function<void()> func;
+    char *stack;
+    bool finished;
+    ucontext_t ctx;
 
-	Routine(std::function<void()> f)
-	{
-		func = f;
-		stack = nullptr;
-		finished = false;
-	}
+    Routine(std::function<void()> f)
+    {
+        func = f;
+        stack = nullptr;
+        finished = false;
+    }
 
-	~Routine()
-	{
-		delete[] stack;
-	}
+    ~Routine()
+    {
+        delete[] stack;
+    }
 };
 
 struct Ordinator
 {
-	std::vector<Routine *> routines;
-	std::list<routine_t> indexes;
-	routine_t current;
-	size_t stack_size;
-	ucontext_t ctx;
+    std::vector<Routine *> routines;
+    std::list<routine_t> indexes;
+    routine_t current;
+    size_t stack_size;
+    ucontext_t ctx;
 
-	inline Ordinator(size_t ss = STACK_LIMIT)
-	{
-		current = 0;
-		stack_size = ss;
-	}
+    inline Ordinator(size_t ss = STACK_LIMIT)
+    {
+        current = 0;
+        stack_size = ss;
+    }
 
-	inline ~Ordinator()
-	{
-		for (auto &routine : routines)
-			delete routine;
-	}
+    inline ~Ordinator()
+    {
+        for (auto &routine : routines)
+            delete routine;
+    }
 };
 
 thread_local static Ordinator ordinator;
 
 inline routine_t create(std::function<void()> f)
 {
-	Routine *routine = new Routine(f);
+    Routine *routine = new Routine(f);
 
-	if (ordinator.indexes.empty())
-	{
-		ordinator.routines.push_back(routine);
-		return ordinator.routines.size();
-	}
-	else
-	{
-		routine_t id = ordinator.indexes.front();
-		ordinator.indexes.pop_front();
-		assert(ordinator.routines[id - 1] == nullptr);
-		ordinator.routines[id - 1] = routine;
-		return id;
-	}
+    if (ordinator.indexes.empty())
+    {
+        ordinator.routines.push_back(routine);
+        return ordinator.routines.size();
+    }
+    else
+    {
+        routine_t id = ordinator.indexes.front();
+        ordinator.indexes.pop_front();
+        assert(ordinator.routines[id - 1] == nullptr);
+        ordinator.routines[id - 1] = routine;
+        return id;
+    }
 }
 
 inline void destroy(routine_t id)
 {
-	Routine *routine = ordinator.routines[id - 1];
-	assert(routine != nullptr);
+    Routine *routine = ordinator.routines[id - 1];
+    assert(routine != nullptr);
 
-	delete routine;
-	ordinator.routines[id - 1] = nullptr;
+    delete routine;
+    ordinator.routines[id - 1] = nullptr;
 }
 
 inline void entry()
 {
-	routine_t id = ordinator.current;
-	Routine *routine = ordinator.routines[id - 1];
-	routine->func();
+    routine_t id = ordinator.current;
+    Routine *routine = ordinator.routines[id - 1];
+    routine->func();
 
-	routine->finished = true;
-	ordinator.current = 0;
-	ordinator.indexes.push_back(id);
+    routine->finished = true;
+    ordinator.current = 0;
+    ordinator.indexes.push_back(id);
 }
 
 inline int resume(routine_t id)
 {
-	assert(ordinator.current == 0);
+    assert(ordinator.current == 0);
 
-	Routine *routine = ordinator.routines[id - 1];
-	if (routine == nullptr)
-		return -1;
+    Routine *routine = ordinator.routines[id - 1];
+    if (routine == nullptr)
+        return -1;
 
-	if (routine->finished)
-		return -2;
+    if (routine->finished)
+        return -2;
 
-	if (routine->stack == nullptr)
-	{
-		//initializes the structure to the currently active context.
-		//When successful, getcontext() returns 0
-		//On error, return -1 and set errno appropriately.
-		getcontext(&routine->ctx);
+    if (routine->stack == nullptr)
+    {
+        //initializes the structure to the currently active context.
+        //When successful, getcontext() returns 0
+        //On error, return -1 and set errno appropriately.
+        getcontext(&routine->ctx);
 
-		//Before invoking makecontext(), the caller must allocate a new stack
-		//for this context and assign its address to ucp->uc_stack,
-		//and define a successor context and assign its address to ucp->uc_link.
-		routine->stack = new char[ordinator.stack_size];
-		routine->ctx.uc_stack.ss_sp = routine->stack;
-		routine->ctx.uc_stack.ss_size = ordinator.stack_size;
-		routine->ctx.uc_link = &ordinator.ctx;
-		ordinator.current = id;
+        //Before invoking makecontext(), the caller must allocate a new stack
+        //for this context and assign its address to ucp->uc_stack,
+        //and define a successor context and assign its address to ucp->uc_link.
+        routine->stack = new char[ordinator.stack_size];
+        routine->ctx.uc_stack.ss_sp = routine->stack;
+        routine->ctx.uc_stack.ss_size = ordinator.stack_size;
+        routine->ctx.uc_link = &ordinator.ctx;
+        ordinator.current = id;
 
-		//When this context is later activated by swapcontext(), the function entry is called.
-		//When this function returns, the  successor context is activated.
-		//If the successor context pointer is NULL, the thread exits.
-		makecontext(&routine->ctx, reinterpret_cast<void(*)(void)>(entry), 0);
+        //When this context is later activated by swapcontext(), the function entry is called.
+        //When this function returns, the  successor context is activated.
+        //If the successor context pointer is NULL, the thread exits.
+        makecontext(&routine->ctx, reinterpret_cast<void(*)(void)>(entry), 0);
 
-		//The swapcontext() function saves the current context,
-		//and then activates the context of another.
-		swapcontext(&ordinator.ctx, &routine->ctx);
-	}
-	else
-	{
-		ordinator.current = id;
-		swapcontext(&ordinator.ctx, &routine->ctx);
-	}
+        //The swapcontext() function saves the current context,
+        //and then activates the context of another.
+        swapcontext(&ordinator.ctx, &routine->ctx);
+    }
+    else
+    {
+        ordinator.current = id;
+        swapcontext(&ordinator.ctx, &routine->ctx);
+    }
 
-	return 0;
+    return 0;
 }
 
 inline void yield()
 {
-	routine_t id = ordinator.current;
-	Routine *routine = ordinator.routines[id - 1];
-	assert(routine != nullptr);
+    routine_t id = ordinator.current;
+    Routine *routine = ordinator.routines[id - 1];
+    assert(routine != nullptr);
 
-	char *stack_top = routine->stack + ordinator.stack_size;
-	char stack_bottom = 0;
-	assert(size_t(stack_top - &stack_bottom) <= ordinator.stack_size);
+    char *stack_top = routine->stack + ordinator.stack_size;
+    char stack_bottom = 0;
+    assert(size_t(stack_top - &stack_bottom) <= ordinator.stack_size);
 
-	ordinator.current = 0;
-	swapcontext(&routine->ctx, &ordinator.ctx);
+    ordinator.current = 0;
+    swapcontext(&routine->ctx, &ordinator.ctx);
 }
 
 inline routine_t current()
 {
-	return ordinator.current;
+    return ordinator.current;
 }
 
 template<typename Function, typename ... Args>
-inline typename std::result_of<Function()>::type
+inline typename std::result_of<Function(Args...)>::type
 await(Function &&func, Args&& ... args)
 {
-	auto future = std::async(std::launch::async, func, std::forward<Args>(args)...);
-	std::future_status status = future.wait_for(std::chrono::milliseconds(0));
+    auto future = std::async(std::launch::async, func, std::forward<Args>(args)...);
+    std::future_status status = future.wait_for(std::chrono::milliseconds(0));
 
-	while (status == std::future_status::timeout)
-	{
-		if (ordinator.current != 0)
-			yield();
+    while (status == std::future_status::timeout)
+    {
+        if (ordinator.current != 0)
+            yield();
 
-		status = future.wait_for(std::chrono::milliseconds(0));
-	}
-	return future.get();
+        status = future.wait_for(std::chrono::milliseconds(0));
+    }
+    return future.get();
 }
 
 #endif
@@ -391,72 +392,72 @@ template<typename Type>
 class Channel
 {
 public:
-	Channel()
-	{
-		_taker = 0;
-	}
+    Channel()
+    {
+        _taker = 0;
+    }
 
-	Channel(routine_t id)
-	{
-		_taker = id;
-	}
+    Channel(routine_t id)
+    {
+        _taker = id;
+    }
 
-	inline void consumer(routine_t id)
-	{
-		_taker = id;
-	}
+    inline void consumer(routine_t id)
+    {
+        _taker = id;
+    }
 
-	inline void push(const Type &obj)
-	{
-		_list.push_back(obj);
-		if (_taker && _taker != current())
-			resume(_taker);
-	}
+    inline void push(const Type &obj)
+    {
+        _list.push_back(obj);
+        if (_taker && _taker != current())
+            resume(_taker);
+    }
 
-	inline void push(Type &&obj)
-	{
-		_list.push_back(std::move(obj));
-		if (_taker && _taker != current())
-			resume(_taker);
-	}
+    inline void push(Type &&obj)
+    {
+        _list.push_back(std::move(obj));
+        if (_taker && _taker != current())
+            resume(_taker);
+    }
 
-	inline Type pop()
-	{
-		if (!_taker)
-			_taker = current();
+    inline Type pop()
+    {
+        if (!_taker)
+            _taker = current();
 
-		while (_list.empty())
-			yield();
+        while (_list.empty())
+            yield();
 
-		Type obj = std::move(_list.front());
-		_list.pop_front();
-		return std::move(obj);
-	}
+        Type obj = std::move(_list.front());
+        _list.pop_front();
+        return std::move(obj);
+    }
 
-	inline void clear()
-	{
-		_list.clear();
-	}
+    inline void clear()
+    {
+        _list.clear();
+    }
 
-	inline void touch()
-	{
-		if (_taker && _taker != current())
-			resume(_taker);
-	}
+    inline void touch()
+    {
+        if (_taker && _taker != current())
+            resume(_taker);
+    }
 
-	inline size_t size()
-	{
-		return _list.size();
-	}
+    inline size_t size()
+    {
+        return _list.size();
+    }
 
-	inline bool empty()
-	{
-		return _list.empty();
-	}
+    inline bool empty()
+    {
+        return _list.empty();
+    }
 
 private:
-	std::list<Type> _list;
-	routine_t _taker;
+    std::list<Type> _list;
+    routine_t _taker;
 };
 
 }
